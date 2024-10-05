554e4adad96d31238cfa4fc5a8c1ff4596ac84ca
Koustubh-lapate <koustubh.lapate@gmail.com>
2024-10-05 11:39:20 +0530

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main(){
+    //1st program function call
     println!("{}", is_even(2707));
 }
 
+//1st program function
 fn is_even(num : i32) -> bool {
     if num % 2 == 0 {
         return true;
